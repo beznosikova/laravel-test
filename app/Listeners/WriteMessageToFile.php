16248ec522556cf018986349496219a863ffd442
beznosikova <tanya.beznosikova@avivi.com.ua>
2018-08-09 19:12:48 +0300

    events and listeners

---
+++ app/Listeners/WriteMessageToFile.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserLoggedIn;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Storage;
+
+class WriteMessageToFile
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  UserLoggedIn  $event
+     * @return void
+     */
+    public function handle(UserLoggedIn $event)
+    {
+        $message = $event->user->name . ' just logged in to the application.'. now();
+        Storage::prepend('loginactivity.txt', $message);
+    }
+}
